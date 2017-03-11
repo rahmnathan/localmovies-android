1533668fdca34cd45e2a47e0a134eb5bbf27dba4
rahmnathan <rahm.nathan@gmail.com>
2017-03-11 16:54:49 -0600

    Removed unneeded imports

--- app/src/main/java/appmain/MovieListAdapter.java
+++ app/src/main/java/appmain/MovieListAdapter.java
@@ -16,9 +16,7 @@ import android.widget.TextView;
 
 import com.rahmnathan.MovieInfo;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import rahmnathan.localmovies.R;
