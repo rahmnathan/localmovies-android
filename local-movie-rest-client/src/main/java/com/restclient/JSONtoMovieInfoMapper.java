b24ba73e45b2958e8c4903723e6c148d774d1977
rahmnathan <rahm.nathan@gmail.com>
2017-03-17 17:27:06 -0500

    Switched from external to internal iterator

--- local-movie-rest-client/src/main/java/com/restclient/JSONtoMovieInfoMapper.java
+++ local-movie-rest-client/src/main/java/com/restclient/JSONtoMovieInfoMapper.java
@@ -13,22 +13,24 @@ class JSONtoMovieInfoMapper {
     List<MovieInfo> jsonArrayToMovieInfoList(JSONArray jsonList) {
         List<MovieInfo> movieInfoList = new ArrayList<>();
 
-        for(int i = 0; i<jsonList.length(); i++) {
-            JSONObject object = jsonList.getJSONObject(i);
-            MovieInfo.Builder builder = MovieInfo.Builder.newInstance()
-                    .setReleaseYear(object.getString("releaseYear"))
-                    .setMetaRating(object.getString("metaRating"))
-                    .setIMDBRating(object.getString("imdbrating"))
-                    .setTitle(object.getString("title"));
+        jsonList.forEach(json -> {
+            JSONObject jsonObject = (JSONObject) json;
 
+            MovieInfo.Builder builder = MovieInfo.Builder.newInstance()
+                    .setReleaseYear(jsonObject.getString("releaseYear"))
+                    .setMetaRating(jsonObject.getString("metaRating"))
+                    .setIMDBRating(jsonObject.getString("imdbrating"))
+                    .setTitle(jsonObject.getString("title"));
+            
             try {
-                builder.setImage(object.getString("image"));
+                builder.setImage(jsonObject.getString("image"));
             } catch (Exception e) {
                 builder.setImage(null);
             }
 
             movieInfoList.add(builder.build());
-        }
+        });
+
         return movieInfoList;
     }
 }
