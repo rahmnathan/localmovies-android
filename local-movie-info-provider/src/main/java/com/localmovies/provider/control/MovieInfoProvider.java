0b30ef0055c4ab1746f1d42ee0aa28e2ac16d451
rahmnathan <rahm.nathan@gmail.com>
2017-03-24 21:24:49 -0500

    Refactoring

--- local-movie-info-provider/src/main/java/com/localmovies/provider/control/MovieInfoProvider.java
+++ local-movie-info-provider/src/main/java/com/localmovies/provider/control/MovieInfoProvider.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,21 +20,28 @@ public class MovieInfoProvider {
 
     public List<MovieInfo> getMovieInfo(Client client, int page, int resultsPerPage) {
         logger.log(Level.INFO, "Requesting movies");
+        JSONArray movieInfoJson = getMovieInfoJson(client, page, resultsPerPage);
+        if(movieInfoJson == null)
+            return new ArrayList<>();
+
+        return movieInfoMapper.jsonArrayToMovieInfoList(movieInfoJson);
+    }
+
+    private JSONArray getMovieInfoJson(Client client, int page, int resultsPerPage){
         String restRequest = "https://" + client.getComputerIP() + ":8443/titlerequest?access_token="
                 + client.getAccessToken() + "&page=" + page + "&resultsPerPage=" + resultsPerPage
                 + "&path=" + client.getCurrentPath().toString().replace(" ", "%20");
         try {
             HttpURLConnection connection = (HttpURLConnection) (new URL(restRequest)).openConnection();
-            if(page == 0)
+            if (page == 0)
                 client.setMovieCount(Integer.valueOf(connection.getHeaderField("Count")));
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder result = new StringBuilder();
             br.lines().forEachOrdered(result::append);
             br.close();
             connection.disconnect();
-
-            return movieInfoMapper.jsonArrayToMovieInfoList(new JSONArray(result.toString()));
-        } catch (Exception e) {
+            return new JSONArray(result.toString());
+        }catch (Exception e) {
             e.printStackTrace();
         }
         return null;
