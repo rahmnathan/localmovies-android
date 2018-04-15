0b100710c79bcd45dc1477dd3b51b4a51e3fd62d
nathan <rahm.nathan@gmail.com>
2018-04-15 14:45:21 -0500

    UI Improvements for new pop-up feature

--- local-movie-app/src/main/java/com/github/rahmnathan/localmovies/app/activity/DescriptionPopUpActivity.java
+++ local-movie-app/src/main/java/com/github/rahmnathan/localmovies/app/activity/DescriptionPopUpActivity.java
@@ -21,7 +21,7 @@ public class DescriptionPopUpActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detaileddescription);
+        setContentView(R.layout.description_popup);
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
