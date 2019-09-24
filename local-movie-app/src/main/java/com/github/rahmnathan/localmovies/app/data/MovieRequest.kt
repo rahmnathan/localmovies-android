ea13334077d652076daee431f02dc02fb4ee502e
nathan <rahm.nathan@gmail.com>
2019-09-23 21:02:20 -0500

    Added order to request.

--- local-movie-app/src/main/java/com/github/rahmnathan/localmovies/app/data/MovieRequest.kt
+++ local-movie-app/src/main/java/com/github/rahmnathan/localmovies/app/data/MovieRequest.kt
@@ -1,6 +1,6 @@
 package com.github.rahmnathan.localmovies.app.data
 
-class MovieRequest(val page: Int, val resultsPerPage: Int, val path: String) {
+class MovieRequest(val page: Int, val resultsPerPage: Int, val path: String, val order:String) {
 
     companion object {
         private val client = "ANDROID"
